6e0312f68b005177aade7ecd435a399bb265d10d
Aloynz <aloysiusneozeen@gmail.com>
2022-09-28 01:02:13 +0800

    Resolve checkstyle issues

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -21,12 +21,12 @@ public class RemarkCommand extends Command {
     public static final String COMMAND_WORD = "remark";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
-    + "by the index number used in the last person listing. "
-    + "Existing remark will be overwritten by the input.\n"
-    + "Parameters: INDEX (must be a positive integer) "
-    + PREFIX_REMARK + "[REMARK]\n"
-    + "Example: " + COMMAND_WORD + " 1 "
-    + PREFIX_REMARK + "Likes to swim.";
+        + "by the index number used in the last person listing. "
+        + "Existing remark will be overwritten by the input.\n"
+        + "Parameters: INDEX (must be a positive integer) "
+        + PREFIX_REMARK + "[REMARK]\n"
+        + "Example: " + COMMAND_WORD + " 1 "
+        + PREFIX_REMARK + "Likes to swim.";
 
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
@@ -69,7 +69,7 @@ public class RemarkCommand extends Command {
      */
     private String generateSuccessMessage(Person personToEdit) {
         String message = !remark.value.isEmpty() ? MESSAGE_ADD_REMARK_SUCCESS : MESSAGE_DELETE_REMARK_SUCCESS;
-        return String.format(message, personToEdit);    }
+        return String.format(message, personToEdit); }
 
     @Override
     public boolean equals(Object other) {
--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for remark command.
+     *
+     * @param remark a string of remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
